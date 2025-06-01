eca1823fec824c3077e945823692edf491921297
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-01 20:08:23 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coba Git Remote</title>
+</head>
+<body>
+  <?php 
+    $greeting = "Hello"
+  ?>
+  <h1><?= $greeting . " " . "Everybody" ?></h1>
+</body>
+</html>
